bde3ad148762d317234df3ccdfaf2fe3637633da
david hernandez <davidhernandez@MacBook-Air-de-david.local>
2021-02-23 00:27:56 -0600

    declare state

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -3,51 +3,55 @@ import MenuItem from '../menu-item/menu-item.component';
 import './directory.styles.scss';
 
 export class Directory extends Component {
-  state = {
-    sections: [
-      {
-        title: 'Collares',
-        imageUrl: 'https://image.freepik.com/vector-gratis/elemento-brillo-capa-geometria-fondo-abstracto-gris_34645-84.jpg',
-        id: 1,
-        size: 'medium',
-        linkUrl: 'shop/hats'
-      },
-      {
-        title: 'Pulseras',
-        imageUrl: 'https://image.freepik.com/vector-gratis/elemento-brillo-capa-geometria-fondo-abstracto-gris_34645-84.jpg',
-        id: 2,
-        size: 'medium',
-        linkUrl: 'shop/jackets'
-      },
-      {
-        title: 'Aretes',
-        imageUrl: 'https://image.freepik.com/vector-gratis/elemento-brillo-capa-geometria-fondo-abstracto-gris_34645-84.jpg',
-        id: 3,
-        size: 'medium',
-        linkUrl: 'shop/jackets'
-      },
-      {
-        title: 'Anillos',
-        imageUrl: 'https://image.freepik.com/vector-gratis/elemento-brillo-capa-geometria-fondo-abstracto-gris_34645-84.jpg',
-        id: 4,
-        size: 'medium',
-        linkUrl: 'shop/sneakers'
-      },
-      {
-        title: 'Tobilleras',
-        imageUrl: 'https://image.freepik.com/vector-gratis/elemento-brillo-capa-geometria-fondo-abstracto-gris_34645-84.jpg',
-        id: 5,
-        size: 'medium',
-        linkUrl: 'shop/womens'
-      },
-      {
-        title: 'Gargantillas',
-        imageUrl: 'https://image.freepik.com/vector-gratis/elemento-brillo-capa-geometria-fondo-abstracto-gris_34645-84.jpg',
-        id: 6,
-        size: 'medium',
-        linkUrl: 'shop/mens'
-      }
-    ]
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      sections: [
+        {
+          title: 'Collares',
+          imageUrl: 'https://image.freepik.com/vector-gratis/elemento-brillo-capa-geometria-fondo-abstracto-gris_34645-84.jpg',
+          id: 1,
+          size: 'medium',
+          linkUrl: 'shop/hats'
+        },
+        {
+          title: 'Pulseras',
+          imageUrl: 'https://image.freepik.com/vector-gratis/elemento-brillo-capa-geometria-fondo-abstracto-gris_34645-84.jpg',
+          id: 2,
+          size: 'medium',
+          linkUrl: 'shop/jackets'
+        },
+        {
+          title: 'Aretes',
+          imageUrl: 'https://image.freepik.com/vector-gratis/elemento-brillo-capa-geometria-fondo-abstracto-gris_34645-84.jpg',
+          id: 3,
+          size: 'medium',
+          linkUrl: 'shop/jackets'
+        },
+        {
+          title: 'Anillos',
+          imageUrl: 'https://image.freepik.com/vector-gratis/elemento-brillo-capa-geometria-fondo-abstracto-gris_34645-84.jpg',
+          id: 4,
+          size: 'medium',
+          linkUrl: 'shop/sneakers'
+        },
+        {
+          title: 'Tobilleras',
+          imageUrl: 'https://image.freepik.com/vector-gratis/elemento-brillo-capa-geometria-fondo-abstracto-gris_34645-84.jpg',
+          id: 5,
+          size: 'medium',
+          linkUrl: 'shop/womens'
+        },
+        {
+          title: 'Gargantillas',
+          imageUrl: 'https://image.freepik.com/vector-gratis/elemento-brillo-capa-geometria-fondo-abstracto-gris_34645-84.jpg',
+          id: 6,
+          size: 'medium',
+          linkUrl: 'shop/mens'
+        }
+      ]
+    }
   }
 
   render() {
